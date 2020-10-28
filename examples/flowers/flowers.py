489432b04bf4c1c6dffcac28d66dce87ac2e185c
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-10-28 16:00:13 -0700

    working on flower's 3d plot

--- examples/flowers/flowers.py
+++ examples/flowers/flowers.py
@@ -2,6 +2,7 @@ import os
 import math
 from knn import KNN
 from ..reader import read_dataset
+from graphs import display_knn_graph
 
 def run():
     # Reading dataset
@@ -14,6 +15,8 @@ def run():
     
     # Predicting
     pred = knn_model.predict(x_test)
-    
-
+ 
+    # Displaying results with plotly 
+    colors_by_label = {'Iris-virginica':'Virginica','Iris-setosa':'Setosa','Iris-versicolor':'Versicolour'}
+    display_knn_graph(x_test,y_train,colors_by_label)
 
