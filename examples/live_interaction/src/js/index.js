0432bf8c0f358a56dad46f9bd206537fd87bfb4d
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-11-16 23:24:33 -0800

    making sketch for a KNN live interaction

---
+++ examples/live_interaction/src/js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",()=>{
+	const dataset_input = document.getElementById("dataset-input"),
+		dataset_csv = document.getElementById("dataset"),
+		scan_dataset = document.getElementById("scan-dataset"),
+		knn_board = document.getElementById("knn-board"),
+		knn_graph = document.getElementById("knn-graph"),
+		knn_classes = document.getElementById("knn-classes");
+
+});
